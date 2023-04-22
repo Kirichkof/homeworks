be6dfa61ccae3e32abbd86250b5a9a706f991c4c
Kirichkof <d-kirichkov@mail.ru>
2023-04-22 16:34:03 +0300

    Добавил конкатенацию в вывод

--- task_1.0.py
+++ task_1.0.py
@@ -2,4 +2,4 @@ year = int(input('Введите год: '))
 if year % 4 == 0 or year % 100 != 0 and year % 400 == 0:
     print (f"Год {year} является високосным")
 else:
-    print (f"Год {year} не является високосным")
+    print ('Год' + year + 'не является високосным')
