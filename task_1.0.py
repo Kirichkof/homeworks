4d9ea09923761ace0f96af933f894da55e7b46a6
Kirichkof <d-kirichkov@mail.ru>
2023-04-24 22:52:01 +0300

    Поменял логику

--- task_1.0.py
+++ task_1.0.py
@@ -1,5 +1,5 @@
 year = int(input('Введите год: '))
-if (year % 4 == 0 and year % 400 == 0) or year % 100 != 0:
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print ('Год ' + str(year) + ' является високосным')
 else:
     print ('Год ' + str(year) + ' не является високосным')
