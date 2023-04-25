28c23dfea9bcc39dcec42fa1bf67683b34276811
Kirichkof <d-kirichkov@mail.ru>
2023-04-25 09:39:44 +0300

    коммит

--- task_1.2.py
+++ task_1.2.py
@@ -1,8 +1,9 @@
-n = input('Введите число: ')
+n = int(input('Введите число: '))
 
-num1 = int(n[0])
-num2 = int(n[1])
-num3 = int(n[2])
-result = num1 + num2 + num3
+a = n % 10
+b = n % 100 // 10
+c = n // 100
 
-print('Сумма чисел числа ' + str(n) + ' равна ' + str(result) )
+result = a + b + c
+
+print('Сумма чисел числа ' + str(n) + ' равна ' + str(result))
